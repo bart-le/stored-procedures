d06e93b7b36d64f21dd8225f292cae15b75b1d2e
Bart Le <bart@pj.edu.pl>
2022-06-04 23:45:24 +0200

    Get first matching Order record

--- warehouses/Services/WarehousesDatabaseService.cs
+++ warehouses/Services/WarehousesDatabaseService.cs
@@ -34,7 +34,39 @@ namespace warehouses.Services
 
 		public async Task<Order> GetOrderAsync(int idProduct, int amount, DateTime createdAt)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				using SqlConnection connection = GetSqlConnection();
+				var command = new SqlCommand(
+					@"SELECT TOP 1 * FROM ""Order"" " +
+					"WHERE IdProduct = @idProduct " +
+					"AND Amount = @amount " +
+					"AND CreatedAt < @createdAt",
+					connection
+				);
+
+				command.Parameters.AddWithValue("@idProduct", idProduct);
+				command.Parameters.AddWithValue("@amount", amount);
+				command.Parameters.AddWithValue("@createdAt", createdAt);
+				await connection.OpenAsync();
+
+				var reader = await command.ExecuteReaderAsync();
+
+				await reader.ReadAsync();
+
+				return new()
+				{
+					IdOrder = Convert.ToInt32(reader["IdOrder"]),
+					IdProduct = Convert.ToInt32(reader["IdProduct"]),
+					Amount = Convert.ToInt32(reader["Amount"]),
+					CreatedAt = Convert.ToDateTime(reader["CreatedAt"]),
+					FulfilledAt = Convert.ToDateTime(reader["CreatedAt"])
+				};
+			}
+			catch (Exception)
+			{
+				return null;
+			}
 		}
 
 		public async Task<bool> OrderCompletedAsync(int idOrder)
