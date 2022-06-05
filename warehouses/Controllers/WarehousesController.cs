822c3d202a3824e85d8f506a99a4c6fbec6d3bba
Bart Le <bart@pj.edu.pl>
2022-06-05 17:00:00 +0200

    Implement product registration controller

--- warehouses/Controllers/WarehousesController.cs
+++ warehouses/Controllers/WarehousesController.cs
@@ -17,8 +17,25 @@ namespace warehouses.Controllers
 		}
 
 		[HttpPost]
-		public async Task<IActionResult> RegisterWarehouseProductAsync(ProductDto productDto)
+		public async Task<IActionResult> RegisterWarehouseProductAsync([FromBody] ProductDto productDto)
 		{
+			if (!await _databaseService.ProductExistsAsync(productDto.IdProduct))
+				return NotFound($"Couldn't find product of ID '{productDto.IdProduct}'");
+
+			var order = await _databaseService
+				.GetOrderAsync(productDto.IdProduct, productDto.Amount, productDto.CreatedAt);
+
+			if (order == null)
+				return NotFound($"Couldn't find order for product of ID '{productDto.IdProduct}'");
+
+			if (await _databaseService.IsOrderCompletedAsync(order))
+				return UnprocessableEntity($"Order of ID {order.IdOrder} has been completed");
+
+			int id = await _databaseService.RegisterWarehouseProductAsync(order.IdOrder, productDto);
+
+			return id > 0
+				? Ok(id)
+				: BadRequest(id);
 		}
 	}
 }
